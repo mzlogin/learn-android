bed301dbfe45bf9e2fe37ec92ac6413896ee8338
Zhuang Ma <ChumpMa@gmail.com>
2014-12-31 15:41:31 +0800

    添加CriminalIntent初始工程,P225

---
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/Crime.java
@@ -0,0 +1,5 @@
+package org.mazhuang.android.criminalintent;
+
+public class Crime {
+
+}
---
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,26 @@
+package org.mazhuang.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+
+public class CrimeActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}

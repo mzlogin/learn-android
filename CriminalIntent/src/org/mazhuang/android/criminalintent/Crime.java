333ee095396333ea4150d02627b9d01b78a85ebd
Zhuang Ma <ChumpMa@gmail.com>
2014-12-31 17:30:28 +0800

    host Fragment on Activity, P248

--- CriminalIntent/src/org/mazhuang/android/criminalintent/Crime.java
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/Crime.java
@@ -1,5 +1,25 @@
 package org.mazhuang.android.criminalintent;
 
-public class Crime {
+import java.util.UUID;
 
+public class Crime {
+	private UUID mId;
+	private String mTitle;
+	
+	public Crime() {
+		// Generate unique identifier
+		mId = UUID.randomUUID();
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public UUID getId() {
+		return mId;
+	}
 }
--- CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package org.mazhuang.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
 
 public class CrimeActivity extends FragmentActivity {
@@ -10,6 +12,16 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction()
+				.add(R.id.fragmentContainer, fragment)
+				.commit();
+		}
 	}
 
 	@Override

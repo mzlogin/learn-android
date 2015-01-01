e5ea662d7c5cd3e0fc6e3a45af3d99dd65012a48
Zhuang Ma <ChumpMa@gmail.com>
2015-01-01 22:09:40 +0800

    Fragment use Bundle to transfer parameters, P321

--- CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeActivity.java
@@ -1,11 +1,15 @@
 package org.mazhuang.android.criminalintent;
 
+import java.util.UUID;
+
 import android.support.v4.app.Fragment;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
-		return new CrimeFragment();
+		UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+		
+		return CrimeFragment.newInstance(crimeId);
 	}
 }
--- CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeFragment.java
@@ -27,7 +27,7 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		UUID crimeId = (UUID)getActivity().getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+		UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
 		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 	}
 	
@@ -71,5 +71,15 @@ public class CrimeFragment extends Fragment {
 		});
 		
 		return v;
+	}
+	
+	public static CrimeFragment newInstance(UUID crimeId) {
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+		
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+		
+		return fragment;
 	}
 }

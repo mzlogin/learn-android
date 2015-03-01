0a79f08c2a15c300c1952b2a182d7e83e20dfef6
Zhuang Ma <ChumpMa@gmail.com>
2015-03-01 16:45:25 +0800

    add a menu, chinese version P252

--- CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeListActivity.java
@@ -8,5 +8,4 @@ public class CrimeListActivity extends SingleFragmentActivity {
 	protected Fragment createFragment() {
 		return new CrimeListFragment();
 	}
-
 }
--- CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/org/mazhuang/android/criminalintent/CrimeListFragment.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,6 +25,7 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 		getActivity().setTitle(R.string.crimes_title);
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
 		
@@ -50,6 +54,17 @@ public class CrimeListFragment extends ListFragment {
 		if (requestCode == REQUEST_CRIME) {
 			
 		}
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.fragment_crime_list, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
 	}
 	
 	private class CrimeAdapter extends ArrayAdapter<Crime> {

149a3cc34f55c412f75b242c2c74a85015be2de0
Zhuang Ma <ChumpMa@gmail.com>
2015-03-12 20:04:36 +0800

    fix multithread UI bug

--- AnimateDemo/src/org/mazhuang/animatedemo/ListItemFlyInOutActivity.java
+++ AnimateDemo/src/org/mazhuang/animatedemo/ListItemFlyInOutActivity.java
@@ -3,11 +3,14 @@ package org.mazhuang.animatedemo;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -15,6 +18,7 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -22,9 +26,27 @@ import android.widget.TextView;
 
 public class ListItemFlyInOutActivity extends Activity {
     
+    private static final String TAG = "ListItemFlyInOutActivity";
+    
     private ExpandableListView mList;
     private Button mClearList;
     private MyExpandableAdapter mListAdapter;
+    
+    private static final int MSG_HANDLER_DELETE_ITEM = 0x01;
+    
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_HANDLER_DELETE_ITEM:
+                    deleteItems();
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +58,7 @@ public class ListItemFlyInOutActivity extends Activity {
             @Override
             public void onChanged() {
                 if (mListAdapter.getGroupCount() != 0) {
-                    deleteItems();
+                    //deleteItems();
                 }
             }
         });
@@ -57,7 +79,8 @@ public class ListItemFlyInOutActivity extends Activity {
             
             @Override
             public void onClick(View v) {
-                deleteItems();
+                //deleteItems();
+                mHandler.sendEmptyMessage(MSG_HANDLER_DELETE_ITEM);
             }
         });
     }
@@ -71,86 +94,82 @@ public class ListItemFlyInOutActivity extends Activity {
         
         final Animation animation = (Animation) AnimationUtils.loadAnimation(ListItemFlyInOutActivity.this, R.anim.fly_left_out);
         animation.setAnimationListener(new AnimationListener() {
-            public void onAnimationStart(Animation animation) {}
+            public void onAnimationStart(Animation animation) {
+            }
  
-            public void onAnimationRepeat(Animation animation) {}
+            public void onAnimationRepeat(Animation animation) {
+            }
  
-            @TargetApi(15)
             public void onAnimationEnd(Animation animation) {
                 mListAdapter.deleteItem();
+                //deleteItems();
+                mHandler.sendEmptyMessage(MSG_HANDLER_DELETE_ITEM);
             }
         });
  
+        Log.d(TAG, "startAnimation " + ((TextView)item).getText());
         item.startAnimation(animation);
     }
     
     class MyExpandableAdapter extends BaseExpandableListAdapter {
 
-        private List<List<String>> mData;
+        private List<String> mGroupData;
+        private List<List<String>> mChildrenData;
         
         public MyExpandableAdapter() {
-            mData = new ArrayList<List<String>>();
+            mGroupData = new ArrayList<String>();
+            mGroupData.add("平台");
+            mGroupData.add("语言");
+            mGroupData.add("设计模式");
             
-            List<String> group1 = new ArrayList<String>();
-            for (int i = 0; i < 3; i ++) {
-                group1.add("hello item" + i);
-            }
-            mData.add(group1);
+            mChildrenData = new ArrayList<List<String>>();
+            List<String> childrenPlatform = new ArrayList<String>();
+            childrenPlatform.add("Linux");
+            childrenPlatform.add("Windows");
+            childrenPlatform.add("Android");
+            childrenPlatform.add("iOS");
+            mChildrenData.add(childrenPlatform);
             
-            List<String> group2 = new ArrayList<String>();
-            for (int i = 0; i < 4; i ++) {
-                group2.add("world item" + i);
-            }
-            mData.add(group2);
+            List<String> childrenLanguage = new ArrayList<String>();
+            childrenLanguage.add("C++");
+            childrenLanguage.add("Java");
+            childrenLanguage.add("Python");
+            childrenLanguage.add("Lisp");
+            childrenLanguage.add("Ruby");
+            mChildrenData.add(childrenLanguage);
             
-            List<String> group3 = new ArrayList<String>();
-            for (int i = 0; i < 5; i ++) {
-                group3.add("ha item" + i);
-            }
-            
-            mData.add(group3);
+            List<String> childrenDesignPattern = new ArrayList<String>();
+            childrenDesignPattern.add("Singleton");
+            childrenDesignPattern.add("Strategy");
+            childrenDesignPattern.add("Bridge");
+            childrenDesignPattern.add("Command");
+            childrenDesignPattern.add("Factory");
+            childrenDesignPattern.add("Adapter");
+            mChildrenData.add(childrenDesignPattern);
         }
         
         @Override
         public int getGroupCount() {
-            if (mData == null) {
-                return 0;
-            } else {
-                return mData.size();
-            }
+            return mGroupData.size();
         }
 
         @Override
         public int getChildrenCount(int groupPosition) {
-            int count = 0;
-            
-            if (mData != null && mData.size() > groupPosition) {
-                if (mData.get(groupPosition) != null) {
-                    count = mData.get(groupPosition).size();
-                }
+            if (mChildrenData.size() > groupPosition) {
+                return mChildrenData.get(groupPosition).size();
+            } else {
+                return 0;
             }
-            
-            return count;
         }
 
         @Override
         public Object getGroup(int groupPosition) {
-            if (mData != null && mData.size() > groupPosition) {
-                return mData.get(groupPosition);
-            } 
-            
-            return null;
+            return mGroupData.get(groupPosition);
         }
 
         @Override
         public Object getChild(int groupPosition, int childPosition) {
-            if (mData != null && mData.size() > groupPosition) {
-                if (mData.get(groupPosition).size() > childPosition) {
-                    return mData.get(groupPosition).get(childPosition);
-                }
-            }
-            
-            return null;                
+            return mChildrenData.get(groupPosition).get(childPosition);
         }
 
         @Override
@@ -174,12 +193,12 @@ public class ListItemFlyInOutActivity extends Activity {
             TextView view = null;
             if (convertView != null) {
                 view = (TextView)convertView;
+                view.setText(mGroupData.get(groupPosition));
                
             } else {
-                view = new TextView(ListItemFlyInOutActivity.this);
+                view = createView(mGroupData.get(groupPosition), true);
             } 
-            view.setBackgroundColor(Color.parseColor("#0000FF"));
-            view.setText("" + groupPosition);
+
             return view;
         }
 
@@ -189,11 +208,11 @@ public class ListItemFlyInOutActivity extends Activity {
             TextView view = null;
             if (convertView != null) {
                 view = (TextView)convertView;
+                view.setText(mChildrenData.get(groupPosition).get(childPosition));
             } else {
-                view = new TextView(ListItemFlyInOutActivity.this);
+                view = createView(mChildrenData.get(groupPosition).get(childPosition), false);
             }
-            view.setBackgroundColor(Color.parseColor("#00FF00"));
-            view.setText("" + mData.get(groupPosition).get(childPosition));
+            
             return view;
         }
 
@@ -203,37 +222,43 @@ public class ListItemFlyInOutActivity extends Activity {
         }
         
         public int getFirstDeletableItemPos() {
-            int pos = 0;
+            int pos = 1;
             
-            if (mData != null && mData.size() > 0) {
-                if (mData.get(0) != null && mData.get(0).size() > 0) {
-                    pos = 1;
+            if (!mChildrenData.isEmpty()) {
+                if (mChildrenData.get(0).isEmpty()) {
+                    pos = 0;
                 }
             }
             
             return pos;
         }
         
-        public int deleteItem() {
-            int pos = -1;
-            if (mData != null && mData.size() > 0) {
-                if (mData.get(0) != null) {
-                    if (mData.get(0).size() > 0) {
-                        mData.get(0).remove(0);
-                        pos = 1;
-                    } else {
-                        mData.remove(0);
-                        pos = 0;
-                    }
-                    notifyDataSetChanged();
-                } else {
-                    mData.remove(0);
-                    pos = 0;
-                    notifyDataSetChanged();
-                }
+        public void deleteItem() {
+            if (mGroupData.isEmpty()) {
+                return;
+            } else if (mChildrenData.get(0).isEmpty()) {
+                mGroupData.remove(0);
+                mChildrenData.remove(0);
+            } else {
+                mChildrenData.get(0).remove(0);
             }
-            
-            return pos;
+            notifyDataSetChanged();
+        }
+        
+        private TextView createView(String text, boolean isGroup) {
+            AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(      
+                    ViewGroup.LayoutParams.MATCH_PARENT, 80);      
+            TextView myText = new TextView(ListItemFlyInOutActivity.this);      
+            myText.setLayoutParams(layoutParams);      
+            myText.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);      
+            myText.setPadding(80, 0, 0, 0);      
+            myText.setText(text);    
+            if (isGroup) {
+                myText.setBackgroundColor(Color.parseColor("#B0D1CA"));
+            } else {
+                myText.setBackgroundColor(Color.parseColor("#76C2E9"));
+            }
+            return myText;    
         }
     }
 }

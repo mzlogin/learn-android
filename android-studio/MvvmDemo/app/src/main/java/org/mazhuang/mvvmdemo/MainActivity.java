232b6414ed771f9e2517cfc09c69197002eea9cb
Zhuang Ma <chumpma@gmail.com>
2018-08-18 10:12:55 +0800

    Add two-way databinding

--- android-studio/MvvmDemo/app/src/main/java/org/mazhuang/mvvmdemo/MainActivity.java
+++ android-studio/MvvmDemo/app/src/main/java/org/mazhuang/mvvmdemo/MainActivity.java
@@ -15,19 +15,7 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         UserEntity user = new UserEntity();
-        user.setAge(30);
-        user.setUsername("mzlogin");
-        user.setNickname("mazhuang");
 
         activityMainBinding.setUser(user);
-
-        user.setAge(18);
-
-        UserEntity user2 = new UserEntity();
-        user2.setUsername("ma");
-        user2.setNickname("Jack");
-        user2.setAge(50);
-
-        activityMainBinding.setUser(user2);
     }
 }
--- android-studio/MvvmDemo/app/src/main/java/org/mazhuang/mvvmdemo/UserEntity.java
+++ android-studio/MvvmDemo/app/src/main/java/org/mazhuang/mvvmdemo/UserEntity.java
@@ -1,14 +1,38 @@
 package org.mazhuang.mvvmdemo;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 import lombok.Data;
+import lombok.Setter;
+
+import org.mazhuang.mvvmdemo.BR;
 
 /**
  * @author mazhuang
  * @date 2018/8/5
  */
-@Data
-public class UserEntity {
+public class UserEntity extends BaseObservable {
     private String username;
-    private String nickname;
-    private int age;
+    private String age;
+
+    @Bindable
+    public String getUsername() {
+        return username;
+    }
+
+    @Bindable
+    public String getAge() {
+        return age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        notifyPropertyChanged(BR.username);
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+        notifyPropertyChanged(BR.age);
+    }
 }
